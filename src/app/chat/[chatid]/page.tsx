f7b1b6f02d5bd75296dad0a6745930d0aada9653
Your Name <your-email@example.com>
2024-05-09 10:03:12 +0300

    Final fix for the chatId bug

--- src/app/chat/[chatid]/page.tsx
+++ src/app/chat/[chatid]/page.tsx
@@ -8,41 +8,32 @@ import { checkSub } from "@/lib/subscription";
 
 type Props = {
   params: {
-    chatId: string;
+    chatid: string;
   };
 };
 
-export default async function chatPage({ params }: Props) {
+export default async function chatPage({ params: { chatid } }: Props) {
   const { userId } = await auth();
-  const { chatId } = params;
   if (!userId) {
     return redirect("/sign-in");
   }
 
   const _chats = await db.select().from(chats).where(eq(chats.userId, userId));
   if (!_chats) {
-    // toast.error("Failed to fetch chats. Please try again later.");
-    console.log("Failed to fetch chats. Please try again later.");
-    
-    // return redirect("/");
+    return redirect("/");
   }
 
-  if (!_chats.find((chat) => chat.id === parseInt(chatId))) {
-    // toast.error("You don't have access to this chat.");
-    console.log("You don't have access to this chat.");
-    console.log("chatId: ", chatId);
-    console.log("params", params);
-    
-    // return redirect("/");
+  if (!_chats.find((chat) => chat.id === parseInt(chatid))) {
+    return redirect("/");
   }
 
-  const currentChat = _chats.find((chat) => chat.id === parseInt(chatId));
+  const currentChat = _chats.find((chat) => chat.id === parseInt(chatid));
   const isPro = await checkSub();
 
   return (
     <ClientChatPage
       isPro={isPro}
-      chatId={parseInt(chatId)}
+      chatId={parseInt(chatid)}
       chats={_chats}
       currentChat={currentChat}
       userId={userId}
